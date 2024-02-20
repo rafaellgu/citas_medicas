d461cec1b2bcc419ba744b8b727dab217a04cc75
Your Name <you@example.com>
2024-02-20 17:32:16 -0500

    Migraciones

---
+++ app/models.py
@@ -0,0 +1,15 @@
+#necesitamos a sglAlchemy:
+#definir los atributos de objeto
+#pero con tipos traducibles a SQL Y mysql
+from app import db 
+
+class Medico(db.Model):
+
+    id = db.Column(db.Integer, primary_key = True ) 
+    nombre =db.Column(db.String(120), nullable = True)
+    apellidos =db.Column(db.String(120), nullable = True)
+    tipo_identificacion=db.Column(db.String(4), nullable = True)
+    numero_identificacion =db.Column(db.Integer)
+    registro_medico =db.Column(db.Integer)
+    especialidad  =db.Column(db.String(50))
+    
