decaeaeb17c84685c57756306197527dc6bcaa36
angiessosa <assosa2@soy.sena.edu.co>
2024-03-13 17:13:57 -0500

    trabajo

--- app/config.py
+++ app/config.py
@@ -4,3 +4,4 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config:
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'datebase.db')
     SQLALCHEMY_TRAK_NOTIFICATIONS = True
+    SECRET_KEY ="123456789"
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 from  . import app, db
 from .models import Medico, Paciente, Consultorio, Cita 
-from flask import render_template, request
+from flask import render_template, request, flash, redirect
 
 #crear ruta para ver el listado de los medicos 
 @app.route("/medicos")
@@ -16,12 +16,12 @@ def get_all_consultorios():
 @app.route("/pacientes")
 def get_all_pacientes():
     pacientes = Paciente.query.all()
-    return render_template("pacientes.html" , pacientes=pacientes )
+    return render_template("pacientes.html" , pacientes = pacientes )
 
 @app.route("/citas")
 def get_all_citas():
     citas = Cita.query.all()
-    return render_template("citase.html" , citas=citas )
+    return render_template("citas.html" , citas=citas )
 
 #crear ruta traer el medico por id (get)
 @app.route("/medicos/<int:id>")
@@ -36,11 +36,11 @@ def get_medico_by_id(id):
 @app.route("/paciente/<int:id>")
 def get_paciente_by_id(id):
     #return "id del medico:" + str(id)
-    #taer el medico ppo id utilizando la entidad Medico
+    #taer el medico ppo id utilizando la entidad Paciente
     paciente = Paciente.query.get(id)
     #y meterlo a una vista
     return render_template("paciente.html", 
-                           p = Paciente)
+                           p = paciente)
 
 #crear una ruta para crear nuevo medico 
 @app.route("/medicos/create", methods =['GET', 'POST'])
@@ -72,7 +72,39 @@ def crate_medico():
         #añadirlo a la sesion sqllchemy
         db.session.add(new_medico)
         db.session.commit()
-        return "medico registrado"
+        flash("Medico registrado correctamente")
+        return redirect("/medicos")
+    
+@app.route("/medicos/update/<int:id>", methods=["POST" , "GET"])
+def update_medico(id):
+    especialidades = [
+            "Cardiologia",
+            "Pediatria",
+            "Ontologia"
+        ]
+    medico_update = Medico.query.get(id)
+    if(request.method == "GET"):
+         return render_template("medico_update.html" , 
+                           medico_update = medico_update,
+                           especialidades = especialidades)
+    elif(request.method == "POST"):
+        #actualizar el medico, con los datos de form
+        medico_update.nombre = request.form["nombre"]
+        medico_update.apellidos = request.form["apellidos"]
+        medico_update.tipo_identificacion = request.form["ti"]
+        medico_update.numero_identificacion = request.form["ni"]
+        medico_update.registro_medico = request.form["rm"]
+        medico_update.especialidad = request.form["es"]
+        db.session.commit()
+        return "medico actualizado"
+    
+@app.route("/medicos/delete/<int:id>")
+def delete_medico(id):
+    medico_delete = Medico.query.get(id)
+    db.session.delete(medico_delete)
+    db.session.commit()
+    return redirect("/medicos")
+    
        
 @app.route("/pacientes/create", methods =['GET', 'POST'])
 def create_paciente():
@@ -131,3 +163,4 @@ def create_consultorio():
         db.session.add(new_consultorio)
         db.session.commit()
         return "consultorio registrado"
+    
